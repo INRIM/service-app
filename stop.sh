30cca25233a57a7c9b5588460635a68d1e68cd56
Alessio Gerace <a.gerace@inrim.it>
2021-07-26 12:24:15 +0200

    [ADD] new script stop

--- stop.sh
+++ stop.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 source ${PWD}/.env
+if [ -z "${MONGO_PORT}" ]; then
+   export MONGO_PORT=27018
+fi
+if [ -z "${SERVER_PORT}" ]; then
+   export SERVER_PORT=8225
+fi
+if [ -z "${CLIENT_PORT}" ]; then
+   export CLIENT_PORT=8526
+fi
 echo "Stop service"
 docker-compose -f docker-compose.yml -p service-app stop
