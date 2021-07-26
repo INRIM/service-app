d8adfe023fa501ee619ab71492fc60abe6b1d476
Alessio Gerace <a.gerace@inrim.it>
2021-07-26 11:21:14 +0200

    [FIX] build scripts

--- build_and_run_dev.sh
+++ build_and_run_dev.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 source .env
+echo "setup ports"
+if [ -z "${MONGO_PORT}" ]; then
+   export MONGO_PORT=27018
+fi
+if [ -z "${SERVER_PORT}" ]; then
+   export SERVER_PORT=8225
+fi
+if [ -z "${CLIENT_PORT}" ]; then
+   export CLIENT_PORT=8526
+fi
 echo "setup mongo"
 if [ -z "${MONGO_INITDB_DATABASE}" ]; then
    echo "MONGO_INITDB_DATABASE env var not set and reuired!"
@@ -25,9 +35,10 @@ if [ ! -e "$PWD/scripts/init_db.js" ]; then
      mkdir scripts
      sh setup_db.sh
 fi
+echo "setup basic user data"
 if [ ! -e "$PWD/backend/ozon/base/data/user.json" ]; then
      sh setup_user.sh
 fi
-echo "Run Build"
+echo "Compose eand Run"
 docker-compose -f docker-compose.yml -p service-app stop
 docker-compose -f docker-compose.yml --env-file .env -p service-app up --force-recreate  --always-recreate-deps --remove-orphans --build
