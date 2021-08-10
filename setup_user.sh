2fce215b61b7ba9e2ad30496104a88525be935ff
Alessio Gerace <a.gerace@inrim.it>
2021-08-10 15:12:17 +0200

    [FIX] user script

--- setup_user.sh
+++ setup_user.sh
@@ -9,7 +9,7 @@ USER='
     "is_admin":true,
     "is_bot":true,
     "nome":"Admin",
-    "mail":"admin@admin.it"
+    "mail":"admin@admin.it",
     "cognome":"Admin",
     "full_name":"Admin Admin",
     "user_function":"user"
