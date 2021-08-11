6656ca3a04c17f9841f920bb5b1920649a69884d
Alessio Gerace <a.gerace@inrim.it>
2021-08-11 10:56:19 +0200

    [IMP] Actions add new ActionProcessTask and ActioinTask Plugin Class [IMP] eval_data tomnage basic Todo List [FIX] minor Fix

---
+++ backend/ozon/core/ServiceActionProcess.py
@@ -0,0 +1,45 @@
+# Copyright INRIM (https://www.inrim.eu)
+# See LICENSE file for full licensing details.
+import sys
+import os
+from os import listdir
+from os.path import isfile, join
+import ujson
+from ozon.settings import get_settings
+from .database.mongo_core import *
+from collections import OrderedDict
+from pathlib import Path
+from fastapi import Request
+from .ServiceSecurity import ServiceSecurity
+from .ServiceAction import ActionMain
+from .ModelData import ModelData
+from .BaseClass import BaseClass, PluginBase
+from pydantic import ValidationError
+from .QueryEngine import QueryEngine
+
+import logging
+import pymongo
+import requests
+import httpx
+import re
+from urllib.parse import quote
+
+logger = logging.getLogger(__name__)
+
+
+class ActionProcessTask(ActionMain):
+
+    @classmethod
+    def create(
+            cls, session: Session, service_main, action_name, rec_name, parent, iframe, execute, pwd_context,
+            container_act=""):
+        self = ActionProcessTask()
+        self.init(session, service_main, action_name, rec_name, parent, iframe, execute, pwd_context,
+                  container_act=container_act)
+        return self
+
+    async def hendle_process_action(self, data={}):
+        pass
+
+    async def hendle_process_task_action(self, data={}):
+        pass
---
+++ backend/ozon/core/ServiceActionTask.py
@@ -0,0 +1,88 @@
+# Copyright INRIM (https://www.inrim.eu)
+# See LICENSE file for full licensing details.
+import sys
+import os
+from os import listdir
+from os.path import isfile, join
+import ujson
+from ozon.settings import get_settings
+from .database.mongo_core import *
+from collections import OrderedDict
+from pathlib import Path
+from fastapi import Request
+from .ServiceSecurity import ServiceSecurity
+from .ServiceActionProcess import ActionProcessTask
+from .ModelData import ModelData
+from .BaseClass import BaseClass, PluginBase
+from pydantic import ValidationError
+from .QueryEngine import QueryEngine
+
+import logging
+import pymongo
+import requests
+import httpx
+import re
+from urllib.parse import quote
+
+logger = logging.getLogger(__name__)
+
+
+class ActionTask(ActionProcessTask):
+
+    @classmethod
+    def create(
+            cls, session: Session, service_main, action_name, rec_name, parent, iframe, execute, pwd_context,
+            container_act=""):
+        self = ActionTask()
+        self.init(session, service_main, action_name, rec_name, parent, iframe, execute, pwd_context,
+                  container_act=container_act)
+        return self
+
+    # helper
+    async def eval_data(self, data={}, eval_todo=True):
+        logger.info(f"{data}")
+        data = await super().eval_data(data=data)
+        if not self.session.is_admin and eval_todo:
+            if "todo" in data:
+                data["todo"] = True
+        return data.copy()
+
+    # actions
+    async def task_action(self, data={}):
+        """
+        :param data:  form data
+        :return: record updated
+        """
+        logger.info(f"task_action -> {self.action.model} action_type {self.action.type}, curr_ref {self.curr_ref}")
+        model_schema = await self.mdata.component_by_name(self.action.model)
+        data_model = await self.mdata.gen_model(self.action.model)
+        record_data = data_model(**data)
+        can_edit = await self.eval_editable(model_schema, record_data)
+        if not can_edit:
+            logger.error(f"Accesso Negato {record_data.rec_name}")
+            return {
+                "status": "error",
+                "message": f"Accesso Negato {record_data.rec_name}",
+                "model": self.action.model
+            }
+        # check if exist task method
+        eval_todo = True
+        if "todo" in self.action.rec_name:
+            eval_todo = False
+            if "todo" in data:
+                data["todo"] = False
+
+        # save record
+        record = await self.save_copy(data=data, eval_todo=eval_todo)
+        # if is error record is dict
+        if isinstance(record, dict):
+            return record
+
+        act_path = await self.compute_action_path(record)
+
+        return {
+            "status": "ok",
+            "link": f"{act_path}",
+            "reload": True,
+            "data": ujson.loads(record.json())
+        }
