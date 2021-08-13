f5f9f8d01258487c10faed2719d5e01d912444ba
Alessio Gerace <a.gerace@inrim.it>
2021-08-13 11:25:23 +0200

    [FIX] config client

--- setup_db.sh
+++ setup_db.sh
@@ -1,8 +1,8 @@
 source .env
 DATA='
 db.createUser({
-  user: "'$MONGO_INITDB_ROOT_USERNAME'",
-  pwd:  "'$MONGO_INITDB_ROOT_PASSWORD'",
-  roles: [ { role: "readWrite", db: "'$MONGO_INITDB_DATABASE'" } ]
+  user: "'$MONGO_USER'",
+  pwd:  "'$MONGO_PASS'",
+  roles: [ { role: "readWrite", db: "'$MONGO_DB'" } ]
 });'
 echo "${DATA}" > scripts/init_db.js
