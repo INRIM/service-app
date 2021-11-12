1c9a1a2a1e39ab216c11ee4e129a6257d552e920
Alessio Gerace <a.gerace@inrim.it>
2021-11-12 18:22:06 +0100

    [FIX] log dir

--- automations/init.sh
+++ automations/init.sh
@@ -3,8 +3,6 @@
 # use systemd-analyze calendar 0/6:*:*
 mkdir /etc/systemd/system/app-services
 mkdir /etc/systemd/system/app-tasks
-mkdir /automations/modules/main-scheduler/log
-mkdir /automations/modules/calendar-task/logs
 cd /automations/modules/delete-record; sh add-unit.sh
 cd /automations/modules/main-scheduler; sh add-unit.sh
 cd /automations
