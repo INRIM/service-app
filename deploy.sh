30edffb3422802c4fc52d6265b11b6566cb82adb
Alessio Gerace <a.gerace@inrim.it>
2021-12-27 11:56:57 +0100

    [FIX] .gitignore

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ if [ -e "$PWD/docker-compose-custom.yml" ]; then
   docker-compose -f docker-compose-custom.yml -p ${STACK} --profile all stop
   docker-compose -f docker-compose-custom.yml -p ${STACK} --profile all --compatibility up --force-recreate  --always-recreate-deps --detach --build
 else
-  echo "setup compose"
+  echo "setup compose ${APP_NAME}"
   docker-compose -f docker-compose.yml --env-file .env -p ${STACK} stop
   docker-compose -f docker-compose.yml --env-file .env -p ${STACK} --compatibility up --force-recreate --always-recreate-deps --detach --build
 fi
