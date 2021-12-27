b216d75aade1fc5af71c9b14971de556e7b033cc
Alessio Gerace <a.gerace@inrim.it>
2021-12-27 11:33:56 +0100

    fix build scripts

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,11 @@ CURRPATH=${PWD}
 ./init.sh
 echo "setup coplete"
 if [ -e "$PWD/docker-compose-custom.yml" ]; then
+  echo "setup compose custom"
   docker-compose -f docker-compose-custom.yml -p ${STACK} --profile all stop
   docker-compose -f docker-compose-custom.yml -p ${STACK} --profile all --compatibility up --force-recreate  --always-recreate-deps --detach --build
 else
+  echo "setup compose"
   docker-compose -f docker-compose.yml --env-file .env -p ${STACK} --profile all stop
-  docker-compose -f docker-compose.yml --env-file .env -p ${STACK} --profile all --compatibility up --force-recreate  --always-recreate-deps --detach --build
+  docker-compose -f docker-compose.yml --env-file .env -p ${STACK} --profile all --compatibility up --force-recreate --always-recreate-deps --detach --build
 fi
