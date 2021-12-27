0ba0cdd5c0f6498adbdbdb52a98a0c08300cd076
Alessio Gerace <a.gerace@inrim.it>
2021-12-27 11:16:50 +0100

    [ADD] project mci services

--- requirements_backend.sh
+++
@@ -1,3 +0,0 @@
-pip install --upgrade pip
-pip install -r /requirements.txt
-pip install -r /apps/backend/inrim/requirements.txt
---
+++ service-app/setup_user.sh
@@ -0,0 +1,20 @@
+source .env
+KEY=$(uuidgen)
+USER='
+[
+  {
+    "rec_name":"'$ADMIN_USERNAME'",
+    "uid":"'$ADMIN_USERNAME'",
+    "password":"'$ADMIN_PASSWORD'",
+    "token":"'$API_USER_KEY'",
+    "is_admin":true,
+    "is_bot":true,
+    "nome":"Admin",
+    "mail":"admin@none.none",
+    "cognome":"Admin",
+    "full_name":"Admin Admin",
+    "user_function":"user"
+  }
+]
+'
+echo "${USER}" > web-client/ozon/base/data/user.json
