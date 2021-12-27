6959b445978b57b225bda9540a28163b5031da89
Alessio Gerace <a.gerace@inrim.it>
2021-12-27 16:58:30 +0100

    [FIX] project and add logs

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ${PWD}/.env
 CURRPATH=${PWD}
-./init.sh
+#./init.sh
 echo "Compose eand Run"
 if [ -e "$PWD/docker-compose-custom.yml" ]; then
   docker-compose -f docker-compose-custom.yml -p ${STACK} --profile core-only stop
--- mongo-cli.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source .env
-if [ -z "${STACK}" ]; then
-   export STACK="service-app"
-fi
-docker exec -ti mongo-${STACK} /bin/bash;
