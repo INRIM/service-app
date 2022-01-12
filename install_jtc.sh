8f1b3900bb1ab05c14f774f61471c2eaeab07845
Alessio Gerace <a.gerace@inrim.it>
2022-01-12 19:38:15 +0100

    [FIX] jtc bin version

--- install_jtc.sh
+++ install_jtc.sh
@@ -5,4 +5,5 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   wget https://github.com/ldn-softdev/jtc/releases/download/LatestBuild/jtc-linux-32.latest -O "${PWD}/jtc"
 fi
+
 chmod a+x "${PWD}/jtc"
