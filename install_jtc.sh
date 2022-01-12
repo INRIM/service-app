24c9cdbe6eb08fdbe6fd15e1d4ae85abd3b9b7cc
Alessio Gerace <a.gerace@inrim.it>
2022-01-12 19:44:09 +0100

    [FIX] project base config

--- install_jtc.sh
+++ install_jtc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "install jtc"
-if [ "$(uname)" == "Darwin" ]; then
+SO="$(uname)"
+if [ $SO == "Darwin" ]; then
   wget https://github.com/ldn-softdev/jtc/releases/download/LatestBuild/jtc-macos-64.latest -O "${PWD}/jtc"
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  wget https://github.com/ldn-softdev/jtc/releases/download/LatestBuild/jtc-linux-32.latest -O "${PWD}/jtc"
+elif [ $SO == "Linux" ]; then
+  wget https://github.com/ldn-softdev/jtc/releases/download/LatestBuild/jtc-linux-64.latest -O "${PWD}/jtc"
 fi
-
 chmod a+x "${PWD}/jtc"
