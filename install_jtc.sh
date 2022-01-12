af33b2c880a16b3a3c021b1fe7421b63f7643624
Alessio Gerace <a.gerace@inrim.it>
2022-01-12 19:50:15 +0100

    [FIX] project base config

--- install_jtc.sh
+++ install_jtc.sh
@@ -3,6 +3,6 @@ echo "install jtc"
 if [ "$OSTYPE"=="darwin"* ]; then
   wget https://github.com/ldn-softdev/jtc/releases/download/LatestBuild/jtc-macos-64.latest -O "${PWD}/jtc"
 elif [ "$OSTYPE"=="linux-gnu"* ]; then
-  wget https://github.com/ldn-softdev/jtc/releases/download/LatestBuild/jtc-linux-64.latest -O "${PWD}/jtc"
+  wget https://github.com/ldn-softdev/jtc/releases/download/LatestBuild/jtc-linux-32.latest -O "${PWD}/jtc"
 fi
 chmod a+x "${PWD}/jtc"
