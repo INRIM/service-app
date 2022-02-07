ca5ea156e323126a24119b6d33c42263a2177352
Alessio Gerace <a.gerace@inrim.it>
2022-02-07 09:34:34 +0100

    [IMP] jinja filters cssid

--- web-client/settings.py
+++ web-client/settings.py
@@ -6,7 +6,16 @@ import config
 
 templates = Jinja2Templates(directory="core/themes")
 
+# setup Jinja2 filters
 
+def cssid(input):
+    """Custom filter"""
+    return f"#{input}"
+
+
+templates.env.filters['cssid'] = cssid
+
+# eval settings and fill cache
 @lru_cache()
 def get_settings():
     return config.SettingsApp()
