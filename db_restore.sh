359c31e51e90953f8b24f9298c8f5f0ec7844938
Alessio Gerace <a.gerace@inrim.it>
2022-02-22 14:52:23 +0100

    [FIX] scripts

--- db_restore.sh
+++ db_restore.sh
@@ -2,5 +2,5 @@
 source .env
 echo "Restore db $MONGO_DB"
 CMD="mongorestore -u ${MONGO_USER} -p ${MONGO_PASS} -d ${MONGO_DB} /dump/${MONGO_DB}"
-docker exec mci-database-1 bash -c "$CMD"
+docker exec ${STACK}-database-1 bash -c "$CMD"
 echo "make project: Done."
