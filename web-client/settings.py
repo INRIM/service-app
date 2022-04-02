6a9708f2f31090000292048ab04ad7d71f8362e5
Alessio Gerace <a.gerace@inrim.it>
2022-04-02 12:32:11 +0200

    [FIX] filters for jinja template

--- web-client/settings.py
+++ web-client/settings.py
@@ -3,8 +3,10 @@
 from pydantic.tools import lru_cache
 from fastapi.templating import Jinja2Templates
 import config
+import json
 
 templates = Jinja2Templates(directory="core/themes")
+
 
 # setup Jinja2 filters
 
@@ -12,12 +14,25 @@ def cssid(input):
     """Custom filter"""
     return f"#{input}"
 
+
 def cssid_div(input):
     """Custom filter"""
     return f"#{input}_div"
 
+
+def parse_json(input):
+    """Custom filter"""
+    res = {}
+    try:
+        return json.loads(input)
+    except Exception as e:
+        return res
+
+
 templates.env.filters['cssid'] = cssid
 templates.env.filters['cssid_div'] = cssid_div
+templates.env.filters['parse_json'] = parse_json
+
 
 # eval settings and fill cache
 @lru_cache()
