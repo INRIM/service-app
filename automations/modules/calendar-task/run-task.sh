9f3890c71fb40dff08ac1aa172f0ab0c232d3d4c
Alessio Gerace <a.gerace@inrim.it>
2022-07-04 16:31:42 +0200

    [FIX] log run task

--- automations/modules/calendar-task/run-task.sh
+++ automations/modules/calendar-task/run-task.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#exec >> /automations/modules/calendar-task/logs/$1.log 2>&1
+exec >> /automations/modules/calendar-task/logs/$1.log 2>&1
 source /automations/modules/.env
 echo "Execute: -X 'POST' 'http://client:8526/client/run/calendar_tasks/$1' -H 'accept: application/json' -H 'apitoken:$API_USER_KEY'"
 echo ""
