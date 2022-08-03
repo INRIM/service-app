489a544d402c1c0c99b671de79f74f7cc0120da9
Alessio Gerace <a.gerace@inrim.it>
2022-08-03 09:35:27 +0200

    [FIX] script

--- dbutils/clean-log.sh
+++ dbutils/clean-log.sh
@@ -5,7 +5,7 @@ exec >> /srv/mci/clean-log.log 2>&1
 
 echo "CLEAN LOG AND DOCKER"
 
-rm -f automations/modules/main-scheduler/log/run_task.log*
-rm -f automations/modules/calendar-task/logs/*.log
+rm -f ../automations/modules/main-scheduler/log/run_task.log*
+rm -f ../automations/modules/calendar-task/logs/*.log
 
 
