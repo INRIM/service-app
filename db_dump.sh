4f1058eca9a2d0eb38d281d9e55a3c0e9ea24d7e
Alessio Gerace <alessio.gerace@gmail.com>
2026-02-12 10:50:38 +0100

    [FIX] scripts

--- db_dump.sh
+++ db_dump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source .env
 echo "dump db $MONGO_DB"
-CMD="mongodump -u $MONGO_USER -p $MONGO_PASS -d $MONGO_DB --out /dump"
+CMD="mongodump -u $MONGO_USER -p xxxxx -d $MONGO_DB --out /dump"
 echo $CMD
 docker exec ${STACK}-database-1 bash -c "${CMD}"
 echo "make project: Done."
